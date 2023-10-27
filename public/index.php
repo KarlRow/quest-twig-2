56619ada8f870366180f393e5b410cb7fc80e57a
Eddy <125514698+KarlRow@users.noreply.github.com>
2023-10-27 09:48:56 +0200

    Ajout d'une variable  + correction html en twig

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@
 require_once __DIR__ . '/../config/twig.php';
 
 
-$name = 'Wilder';
+$name = 'Eddy';
+$name2 = 'Vincent';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('home.html.twig', ['name' => $name, 'name2' => $name2]);
+
+
